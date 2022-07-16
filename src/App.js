14a2cb6751453425b1d2706fdc05d0ea78906e01
OstriySous <boyinv@yandex.ru>
2022-07-16 17:18:06 +0300

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
 
-import './scss/app.scss';
+import pizzas from './assets/pizzas.json'
+import './scss/app.scss'
 
 function App() {
   return (
@@ -18,7 +19,13 @@ function App() {
             <Sort />
           </div>
           <h2 className="content__title">Все пиццы</h2>
-          <PizzaBlock title ="Мексиканская" price="500"/>
+                <div className="content_itmes">
+                {pizzas.map((object) => (
+                    <PizzaBlock
+                        {... object}
+                    />
+                ))}
+            </div>
         </div>
       </div>
     </div>
