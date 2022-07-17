abaae11021c9cd246d99b8c18b11c105e3b1d492
OstriySous <boyinv@yandex.ru>
2022-07-17 18:03:48 +0300

    update

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 function Sort() {
     const [open, setOpen] = React.useState(0)
+    const [selected, setSelected] = React.useState(0)
+    const list = ['популярности', 'цене', 'алфавиту']
+    const sortName = list[selected]
+
+    const onClickListItem = (i) => {
+        setSelected(i)
+        setOpen(false)
+    }
 
   return (
     <div className="sort">
@@ -19,19 +27,24 @@ function Sort() {
                   />
                 </svg>
                 <b>Сортировка по:</b>
-                <span onClick={() => setOpen(true)}>популярности</span>
+                <span onClick={() => setOpen(!open)}>{list[sortName]}</span>
               </div>
                   {open && (
                       <div className="sort__popup">
                           <ul>
-                              <li className="active">популярности</li>
-                              <li>цене</li>
-                              <li>алфавиту</li>
+                          {list.map((name, i) => (
+                              <li
+                              key={i}
+                              onClick={() => onClickListItem(i)}
+                              className={selected === i ? 'active' : ''}>
+                                  {name}
+                              </li>
+                          ))}
                           </ul>
                     </div>
                       )}
             </div>
-  )
+  );
 }
 
 export default Sort;
